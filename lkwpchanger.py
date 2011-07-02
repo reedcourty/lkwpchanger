78f60b1672e495b19b17da3de94b7e3ff8b686c8
Gyorgy Nadudvari <reedcourty@gmail.com>
2011-07-02 23:52:49 +0200

    Added delay and version parameters handling.

--- lkwpchanger.py
+++ lkwpchanger.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-print("Hello World!")
+import argparse
+
+VERSION = '0.0.0.20110702'
+
+if __name__ == "__main__":
+    print("LK Wallpaper Changer mostly for Gabi's WinXP")
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--delay', action='store', dest='delay',
+                    help='Delay')
+    
+    parser = argparse.ArgumentParser(version=VERSION)
+    
+    results = parser.parse_args()
+    print 'delay =', results.delay
+
