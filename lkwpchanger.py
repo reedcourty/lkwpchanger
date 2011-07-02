b5b0c0bffa848ee20f3e4c987303dd65a14d4813
Gyorgy Nadudvari <reedcourty@gmail.com>
2011-07-03 00:58:37 +0200

    I've made some mistakes. Now I added version and delay parameters (both long and short aliases too).

--- lkwpchanger.py
+++ lkwpchanger.py
@@ -3,17 +3,17 @@
 
 import argparse
 
-VERSION = '0.0.0.20110702'
+VERSION = '0.0.0.20110703'
 
 if __name__ == "__main__":
     print("LK Wallpaper Changer mostly for Gabi's WinXP")
     
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--delay', action='store', dest='delay',
+    parser.add_argument('--delay', '-d', action='store', dest='delay',
                     help='Delay')
     
-    parser = argparse.ArgumentParser(version=VERSION)
+    parser.add_argument('--version', '-v', action='version', version='%(prog)s '+VERSION)
     
     results = parser.parse_args()
     print 'delay =', results.delay
